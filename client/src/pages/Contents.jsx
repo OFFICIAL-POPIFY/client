c77e2fbaa68baf694701dbbcbe98f521c3544904
bizbaeja <bjgbjg112@gmail.com>
2023-08-04 20:31:56 +0900

    Feat : Component 추가

--- client/src/pages/Contents.jsx
+++ client/src/pages/Contents.jsx
@@ -1,8 +1,14 @@
 import React from "react";
 import Carousel from "../components/Carousel";
+import RollingBanner from "../components/RollingBanner";
+import Top from "../components/Top";
+import Map from "../components/Map";
 function Contents() {
   return <div>
+    <Top />
+    <RollingBanner />
     <Carousel />
+    <Map />
   </div>;
 }
 
