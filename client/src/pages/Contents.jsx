02785f5f352426eccdebe73d273efc50210bb706
bizbaeja <bjgbjg112@gmail.com>
2023-08-07 00:19:14 +0900

    Fix : 불필요한 코드 정리

--- client/src/pages/Contents.jsx
+++ client/src/pages/Contents.jsx
@@ -2,13 +2,12 @@ import React from "react";
 
 import NewCarousel from "../components/NewCarousel";
 import MapContainer from "../components/MapContainer";
-import Review from "../components/Review";
+
 function Contents() {
   return (
     <div>
       <NewCarousel />
       <MapContainer />
-      <Review />
     </div>
   );
 }
