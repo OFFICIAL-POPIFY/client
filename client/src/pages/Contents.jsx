ca66e17c8f4e5dc08833f669ecf063fdcc8a4289
Dex <136880556+Jihoonnnn@users.noreply.github.com>
2023-08-07 01:05:59 +0900

    Feat : CommentForm 추가

--- client/src/pages/Contents.jsx
+++ client/src/pages/Contents.jsx
@@ -3,12 +3,15 @@ import React from "react";
 import Review from "../components/Review";
 import Carousel from "../components/Carousel";
 import MapContainer from "../components/MapContainer";
+import CommentForm from "../components/CommentForm";
+
 function Contents() {
   return (
     <div>
       <Review />
       <Carousel />
       <MapContainer />
+      <CommentForm />
     </div>
   );
 }
