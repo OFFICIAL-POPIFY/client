33fe5902c4b4bcf1c7e3aff2015049acfb625b57
bizbaeja <bjgbjg112@gmail.com>
2023-08-06 20:31:24 +0900

    Chore : Review 스타일링 및

--- client/src/components/Review.jsx
+++ client/src/components/Review.jsx
@@ -5,71 +5,51 @@ import classes from "./Review.module.css";
 function Review() {
   const titleRef = useRef();
   const bodyRef = useRef();
-  const fileUrlRef = useRef();
   const [posts, setPosts] = useState([]);
   const [editPostId, setEditPostId] = useState(null);
   const [formData, setFormData] = useState({
     title: "",
     bodyUrl: "",
-    fileUrl: "",
   });
-  const writeNewPost = (userId, title, bodyUrl, fileUrl) => {
+  const writeNewPost = (userId, title, bodyUrl) => {
     const newPost = {
       key: Date.now().toString(),
       userId: userId,
       title: title,
       bodyUrl: bodyUrl,
-      fileUrl: fileUrl,
     };
     const updatedPosts = [...posts, newPost];
     setPosts(updatedPosts);
     localStorage.setItem("posts", JSON.stringify(updatedPosts));
   };
 
-  const handleImageUpload = (e, files) => {
-    e.preventDefault();
-    console.log("Image upload logic goes here");
-  };
-
   const updatePost = (postId, title, bodyUrl, fileUrl) => {
     const updatedPosts = posts.map((post) =>
-      post.key === postId
-        ? { ...post, title: title, bodyUrl: bodyUrl, fileUrl: fileUrl }
-        : post
+      post.key === postId ? { ...post, title: title, bodyUrl: bodyUrl } : post
     );
     setPosts(updatedPosts);
     localStorage.setItem("posts", JSON.stringify(updatedPosts));
-  };
-
-  const handleImageChange = (e) => {
-    const files = Array.from(e.target.files);
-    const fileUrls = files.map((file) => URL.createObjectURL(file));
-    setFileUrls(fileUrls);
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
     const title = titleRef.current.value;
     const bodyUrl = bodyRef.current.value;
-    const fileUrl = fileUrlRef.current.value;
 
     if (editPostId) {
-      updatePost(editPostId, title, bodyUrl, fileUrl);
+      updatePost(editPostId, title, bodyUrl);
     } else {
-      writeNewPost("user_id", title, bodyUrl, fileUrl);
+      writeNewPost("user_id", title, bodyUrl);
     }
 
     titleRef.current.value = "";
     bodyRef.current.value = "";
-    fileUrlRef.current.value = "";
 
     setFormData({
       title: "",
       bodyUrl: "",
-      fileUrl: "",
     });
 
-    setFileUrls([]);
     setEditPostId(null);
   };
 
@@ -84,12 +64,9 @@ function Review() {
     if (postToEdit) {
       titleRef.current.value = postToEdit.title;
       bodyRef.current.value = postToEdit.bodyUrl;
-      fileUrlRef.current.value = postToEdit.fileUrl;
       setEditPostId(postId);
     }
   };
-
-  const [fileUrls, setFileUrls] = useState([]);
 
   useEffect(() => {
     const storedPosts = localStorage.getItem("posts");
@@ -101,6 +78,7 @@ function Review() {
   return (
     <div>
       <main className={classes.auth}>
+        <h1>리뷰</h1>
         <div className={classes.wrapper}>
           <form className={classes.form} onSubmit={handleSubmit}>
             <div className={classes.control}>
@@ -113,36 +91,10 @@ function Review() {
             </div>
             <div className={classes.control}>
               <label htmlFor="body">본문</label>
-              <input id="text" className={classes.image} ref={bodyRef}></input>
+              <input id="body" className={classes.body} ref={bodyRef}></input>
             </div>
 
-            <div className={classes.control}>
-              {/* 여기서부터 */}
-              <div>
-                <form onSubmit={(e) => handleImageUpload(e, fileUrls)}>
-                  <label>
-                    파일:
-                    <input
-                      multiple
-                      accept="image/*"
-                      type="file"
-                      onChange={handleImageChange}
-                      ref={fileUrlRef}
-                    />
-                  </label>
-                </form>
-                {fileUrls?.length > 0 && (
-                  <ul>
-                    {fileUrls.map((url, index) => (
-                      <li key={index}>
-                        <img src={url} alt="사용자 첨부 이미지" />
-                      </li>
-                    ))}
-                  </ul>
-                )}
-              </div>
-              {/* 여기까지 */}
-            </div>
+            <div className={classes.control}></div>
             <button className={classes.button} type="submit">
               {editPostId ? "수정" : "제출"}
             </button>
@@ -157,7 +109,6 @@ function Review() {
                 <li key={post.key}>
                   <h3>{post.title}</h3>
                   <img src={post.picture} alt={post.title} />
-
                   <button onClick={() => handleDelete(post.key)}>삭제</button>
                   <button onClick={() => handleEdit(post.key)}>수정</button>
                 </li>
--- client/src/pages/Contents.jsx
+++ client/src/pages/Contents.jsx
@@ -2,12 +2,13 @@ import React from "react";
 
 import NewCarousel from "../components/NewCarousel";
 import MapContainer from "../components/MapContainer";
-
+import Review from "../components/Review";
 function Contents() {
   return (
     <div>
       <NewCarousel />
       <MapContainer />
+      <Review />
     </div>
   );
 }
