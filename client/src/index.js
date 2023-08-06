b182ad30716c8f9f094d8ca947f0e2850bacf787
bizbaeja <bjgbjg112@gmail.com>
2023-08-07 01:14:35 +0900

    Feat : private route 구현

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter,Route,Routes } from 'react-router-dom';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './store';
 import { AuthProvider } from '../src/context/AuthProvider';
@@ -12,30 +11,27 @@ import Mypage from '../src/pages/Mypage';
 import LoginPage from '../src/pages/LoginPage';
 import SignupPage from './pages/SignupPage';
 import App from '../src/App';
-import Auth from './store/auth';
+import PrivateRoute from './PrivateRoute';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}> 
-    <BrowserRouter>
-   <AuthProvider>
-    <Routes>
-    <Route path='/'element={<App />}>
-    <Route path="/about" element={<About />} />
-    <Route path="/contents" element={<Contents />} />
-    <Route path="/signup" element={<SignupPage />} />
-    <Route path="/mypage" element={<Mypage />} />
-    <Route path="/login" element={<LoginPage />} />
-    </Route>
-  
-    </Routes>
-    </AuthProvider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AuthProvider>
+          <Routes>
+            <Route path='/' element={<App />}>
+              <Route path="/about" element={<About />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            <Route path="/contents" element={<Contents />} />
+            </Route>
+            <Route path="/mypage" element={<PrivateRoute element={<Mypage />} />} />
+          </Routes>
+        </AuthProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// ...
