72a687a3346115ccd4db5200fad32606829dc993
bizbaeja <bjgbjg112@gmail.com>
2023-08-08 10:13:42 +0900

    Docs : 데이터 추가

--- client/src/pages/Contents.jsx
+++ client/src/pages/Contents.jsx
@@ -3,7 +3,6 @@ import Thumbnail from "../components/Thumbnail";
 import NewCarousel from "../components/NewCarousel";
 import MapContainer from "../components/MapContainer";
 import CommentForm from "../components/CommentForm";
-
 function Contents() {
   return (
     <div>
@@ -16,4 +15,3 @@ function Contents() {
 }
 
 export default Contents;
-
