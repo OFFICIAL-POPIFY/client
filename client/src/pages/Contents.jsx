cd00d3c50db4116ee0e1fcefe33a620da80452ce
Jhee Seo In <banny19@naver.com>
2023-08-08 09:23:59 +0900

    Fix  : 충돌 해결

--- client/src/pages/Contents.jsx
+++ client/src/pages/Contents.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import Thumbnail from "../components/Thumbnail";
 import NewCarousel from "../components/NewCarousel";
-<<<<<<< HEAD
-=======
-
->>>>>>> 54986a39306cc70b9eb803185b44236a17cc3088
 import MapContainer from "../components/MapContainer";
 import CommentForm from "../components/CommentForm";
 
