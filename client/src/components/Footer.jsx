fa87bfe79d917bdb0d935e9315dccb408c31fe69
Jhee Seo In <banny19@naver.com>
2023-08-16 16:23:47 +0900

    Chore : Footer 및 RollingBanner CSS 수정

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { BsInstagram } from "react-icons/bs";
 import "../App.css";
 function Footer() {
   return (
@@ -8,9 +10,14 @@ function Footer() {
         <div>INFINITE MOMENTS FLOW FROM PLACES THAT ARE NOT FOREVER</div>
         <p>영원하지 않은 공간으로부터의 무한한 순간들</p>
       </Subscript>
-      <img src="./images/logo2.png" alt="" />
-      <span>FOLLOW</span>
-      <span>icon</span>
+      <img src="./images/logo2.png" alt="logo2" />
+      <div className="div1">FOLLOW</div>
+      <Link to="https://www.instagram.com/popify.official/">
+        <div className="div2">
+          <BsInstagram />
+        </div>
+      </Link>
+      <span id="copy">Copyright &copy; ESG Project All rights reserved. Website by. POPIFY</span>
     </Wrapper>
   );
 }
@@ -24,17 +31,20 @@ const Wrapper = styled.div`
   align-items: center;
   line-height: 50rem;
   padding-top: 50px;
-
+  position:relative
   background: #000;
   flex-direction: column;
-  span {
+  .div1 {
+    position:absolute;
+    top:54%;
     display: flex;
     width: 61px;
     height: 20px;
     flex-direction: column;
-    justify-content: center;
+
     flex-shrink: 0;
     color: #fff;
+    padding-right:4rem;
     text-align: center;
     font-family: Pretendard;
     font-size: 15px;
@@ -42,10 +52,38 @@ const Wrapper = styled.div`
     font-weight: 500;
     line-height: normal;
   }
+  .div2 {
+    position:absolute;
+    top:54%;
+    padding-left:2rem;
+    display: flex;
+    color: #fff;
+    width: 20px;
+    height: 20px;
+    flex-shrink: 0;
+  }
   img {
     margin-top: 50px;
     width: 350px;
     height: 120px;
+  }
+
+  #copy{
+    position:absolute;
+    top:70%;
+    display: flex;
+    width: 100%;
+    height: 20px;
+    flex-direction: column;
+    justify-content: center;
+    flex-shrink: 0;
+    color: #959595;
+    text-align: center;
+    font-family: Pretendard;
+    font-size: 15px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
   }
 `;
 const Subscript = styled.div`
@@ -72,4 +110,14 @@ const Subscript = styled.div`
     font-weight: 400;
     line-height: normal;
   }
+`;
+
+const copy = styled.div`
+color: #959595;
+text-align: center;
+font-family: Pretendard;
+font-size: 15px;
+font-style: normal;
+font-weight: 400;
+line-height: normal;
 `;
