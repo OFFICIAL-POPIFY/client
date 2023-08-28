f87f78b5c9f47d9a9d952f11209dbf749a534cd0
Jhee Seo In <banny19@naver.com>
2023-08-28 14:53:17 +0900

    Chore: 선 CSS 변경

--- client/src/components/MyReview.jsx
+++ client/src/components/MyReview.jsx
@@ -37,7 +37,7 @@ function MyReview() {
         <ul>
           {reviews.map((review) => (
             <li key={review.id}>
-              <div>
+              <div className={classes.review}>
                 <div className={classes.icon}>
               <TiDocumentText size="25" />
               </div>
@@ -64,9 +64,10 @@ const Review = styled.div`
   ul {
     list-style: none;
     padding: 0;
+  
   }
   li {
-    border: 1px solid #ccc;
+    border: 0.5px solid #ccc;
     border-radius: 0.3125rem;
     padding: 10px;
   }
