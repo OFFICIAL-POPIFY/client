c36030e74f1eabab48d49ff68e7ebc42ad6055ef
bizbaeja <75407823+bizbaeja@users.noreply.github.com>
2023-09-05 09:36:13 +0900

    Feat : 연결1

--- client/src/components/header/SearchBar.jsx
+++ client/src/components/header/SearchBar.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import classes from "./SearchBar.module.css";
 import axios from "../../api/axios";
 import { Link } from "react-router-dom";
@@ -7,22 +7,29 @@ function SearchBar({ placeholder, data }) {
   const corporation = { placeholder };
   const FILTER_URL = `${process.env.REACT_APP_BASE_URL}/popups/search?corporation=${corporation}`;
   const [filteredData, setFilteredData] = useState([]);
-  const [resData, setResData] = useState(null); // 추가
+  const [resData, setResData] = useState([]);
+  const [selectedId, setSelectedId] = useState("");
 
-  // FILTER_URL에 대한 axios 요청
-  const handleFilter = async (event) => {
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const res = await axios.get(FILTER_URL);
+        setResData(res.data);
+        console.log("response", res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchData();
+  }, [FILTER_URL]);
+
+  const handleFilter = (event) => {
     const searchWord = event.target.value;
-    try {
-      const res = await axios.get(FILTER_URL);
-      setFilteredData(res.data);
-      setResData(res.data); // res 값을 상태로 저장
-      console.log("response", res.data);
-    } catch (error) {
-      console.log(error);
-    }
 
     if (!searchWord) {
       setFilteredData([]);
+      setSelectedId("");
       return;
     }
 
@@ -31,27 +38,29 @@ function SearchBar({ placeholder, data }) {
     });
 
     setFilteredData(newFilter);
+
+    const matchingData = resData.find((item) =>
+      item.corporation.includes(searchWord)
+    );
+
+    setSelectedId(matchingData?._id);
   };
-  console.log("resData1", resData[0]._id);
 
   return (
     <div className={classes.container}>
       <input type="text" placeholder={placeholder} onChange={handleFilter} />
       <div className={classes.search}></div>
-      {filteredData.length !== 0 &&
-        (console.log("filteredData", filteredData),
-        (
-          <div className={classes.data_result}>
-            {filteredData.map((value, key) => (
-              <div key={key}>
-                <Link to={`/popups/search/${resData[0]._id}`}>
-                  {/* resData를 사용 */}
-                  <p>{value.corporation}</p>
-                </Link>
-              </div>
-            ))}
-          </div>
-        ))}
+      {filteredData.length !== 0 && (
+        <div className={classes.data_result}>
+          {filteredData.map((value, key) => (
+            <div key={key}>
+              <Link to={`/popups/search/${selectedId}`}>
+                <p>{value.corporation}</p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 }
