fad82d84091c109ea25af767dc86b6af62d87ce3
Dex <136880556+Jihoonnnn@users.noreply.github.com>
2023-09-06 10:22:53 +0900

    Test: commit test

--- client/src/components/Board.jsx
+++ client/src/components/Board.jsx
@@ -6,22 +6,29 @@ function Board() {
   const storeId = window.location.pathname.split("/")[3];
   const CORPORATION_URL = `${process.env.REACT_APP_BASE_URL}/popups/search/${storeId}`;
   const [corporationData, setCorporationData] = useState([]);
-
+  const [corporationContents, setCorporationContents] = useState("");
   useEffect(() => {
     axios
       .get(CORPORATION_URL)
       .then((response) => {
         setCorporationData(response.data);
+        setCorporationContents(response.data.contents);
+        console.log("resData",response.data.contents)
       })
       .catch((error) => {
         console.log(error);
       });
   }, [CORPORATION_URL]);
-  console.log(corporationData);
+  
   
   const corporationItems = (
-      <span>{corporationData.contents}</span>
+    <>
+    <img src={corporationContents} alt="corporation" />
+      {/*<span>{corporationData.contents}</span>*/}
+      </>
   );
+
+  console.log('item',corporationItems);
   return <Wrap>{corporationItems}</Wrap>;
 }
 
